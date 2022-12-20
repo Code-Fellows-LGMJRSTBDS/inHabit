7320e80dd96e59accaeab18ff63847f6789991a0
TYLER BENNETT <tyler.bennett52@gmail.com>
2022-12-20 16:13:08 -0600

    condense date range into single HTML Element and set copy via string template literal

--- js/home.js
+++ js/home.js
@@ -108,14 +108,6 @@ if (oldHabits) {
     }
   }
 }
-
-
-function deleteItems() {
-  // Clear localStorage items 
-  localStorage.clear();
-};
-
-
 let curr = new Date; // get current date
 let first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
 let last = first + 6; // last day is the first day + 6
@@ -123,5 +115,4 @@ let last = first + 6; // last day is the first day + 6
 let firstday = new Date(curr.setDate(first)).toUTCString().slice(0, 17);
 let lastday = new Date(curr.setDate(last)).toUTCString().slice(0, 17);
 
-document.getElementById('date').innerHTML = firstday;
-document.getElementById('dates').innerHTML = lastday;
+document.getElementById('date').innerHTML = `${firstday} - ${lastday}`;
