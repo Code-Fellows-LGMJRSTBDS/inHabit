21ab924a7d947111919e8079e7a76828ae7eacab
Rafael Aldana <rafaelaldana6@Gmail.com>
2022-12-20 08:36:22 -0800

    added notes for possible month datachart

--- js/stats.js
+++ js/stats.js
@@ -69,3 +69,23 @@ function renderChart() {
 
 // ******************** EXECUTABLE CODE **************************
 renderChart();
+
+
+
+
+// month constructor
+// get the month 
+// properties actual month
+// new Date().getmonth()
+// need properties for tasks added and completed
+// wherever in the code the tasks are being added and completed tap into that
+// on page load create(if youre new to the page create of the months/ if not use that data stored to pull the data from the local strorage ) all of the months but also needs the all of the tasks and completed tasks
+// create an array for the year
+
+
+
+// global current month = to new Date().getMonth()
+
+// create a constructor of all months array that'll stores months created form the constructor 
+// properties on there the month where you'd type of january give a value and a month string (month name) and a property for tasks added and completed 
+// where-ever the tasks are being added to the day or complted do the same at the global month constructor comparing it to the month object 
