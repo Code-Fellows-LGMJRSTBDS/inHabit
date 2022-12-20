ed950f7f863a53e51ebe0aae96b78e34e247e6f1
cloudnalu <68409311+cloudnalu@users.noreply.github.com>
2022-12-20 10:10:15 -0700

    c

--- js/nav-toggle.js
+++
@@ -1,8 +0,0 @@
-function classToggle() {
-  const navs = document.querySelectorAll('.nav-items-right')
-  
-  navs.forEach(nav => nav.classList.toggle('nav-toggle'));
-}
-
-document.querySelector('.Navbar__Link-toggle')
-  .addEventListener('click', classToggle);
