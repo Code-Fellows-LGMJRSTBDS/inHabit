d843f5b9bec5f216c70cd9994d1f072d85d6c664
cloudnalu <68409311+cloudnalu@users.noreply.github.com>
2022-12-20 11:31:47 -0700

    JS from freecode camp but doesnt work for toggle

--- js/menu-toggle.js
+++ js/menu-toggle.js
@@ -1,7 +1,16 @@
 //Get the button, and when the user clicks on it, execute myFunction
 
-document.getElementById("nav-toggle").onclick = function() {toggleNav()};
-/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
-function toggleNav() {
-  document.getElementById("nav-items").classList.toggle("nav-toggle-show");
+// document.getElementById("nav-toggle").onclick = function() {toggleNav()};
+// /* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+// function toggleNav() {
+//   document.getElementById("nav-items").classList.toggle("nav-toggle-show");
+// }
+
+function classToggle() {
+  const navs = document.querySelectorAll('.dropdown-items')
+  
+  navs.forEach(nav => nav.classList.toggle('nav-toggle-show'));
 }
+
+document.querySelector('.nav-toggle')
+  .addEventListener('click', classToggle);
